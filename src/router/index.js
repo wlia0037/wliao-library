5cbae92f9e6596075c902963b29487f36f87256d
wlia0037 <wlia0037@student.monash.edu>
2025-10-07 18:16:00 +1100

    Lab set up, Create FirebaseSigninView.vue, and import the component

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,12 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import AboutView from '../views/AboutView.vue'
+import FirebaseSigninView from '@/views/FirebaseSigninView.vue'
 
 const routes = [
   { path: '/', name: 'Home', component: HomeView },
-  { path: '/about', name: 'About', component: AboutView }
+  { path: '/about', name: 'About', component: AboutView },
+  { path: '/FireLogin', name: 'FireLogin', component: FirebaseSigninView }
 ]
 
 const router = createRouter({
